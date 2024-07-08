eebc404c07890d7ec6e6bcf42e64e6e7280b8833
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-07-08 12:13:10 +0530

    quick 1212

--- Sort/Quick.java
+++ Sort/Quick.java
@@ -1,6 +1,19 @@
 package Sort;
 
 public class Quick {
+
+    public static int partition(int nums[],int low,int high,int pivot)
+    {
+        for(int j=low;j<high;j++){
+            if(nums[j]<pivot){
+                i++;
+                swap(nums[i],nums[j]);
+            }
+        }
+        swap(nums[i+1],nums[high]);
+
+        return i+1;
+    }
     public static void main(String[] args) {
         int nums[] = { 6, 5, 2, 8, 9, 4 };
         int size = nums.length;
@@ -9,9 +22,7 @@ public class Quick {
         for (int n : nums) {
             System.out.print(n + " ");
         }
-
-        
-
+    
         System.out.println();
         System.out.println("After Sorting");
         for (int n : nums) {
