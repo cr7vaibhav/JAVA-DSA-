a1bd8a22ce96701b3d605744437e262014279876
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-07-08 11:23:42 +0530

    insertion 1123

---
+++ Sort/Quick.java
@@ -0,0 +1,21 @@
+package Sort;
+
+public class Quick {
+    public static void main(String[] args) {
+        int nums[] = { 6, 5, 2, 8, 9, 4 };
+        int size = nums.length;
+        
+        System.out.println("Before Sorting");
+        for (int n : nums) {
+            System.out.print(n + " ");
+        }
+
+        
+
+        System.out.println();
+        System.out.println("After Sorting");
+        for (int n : nums) {
+            System.out.print(n + " ");
+        }
+    }
+}
