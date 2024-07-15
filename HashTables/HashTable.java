1f0a80f3503e2279de2507979c4c507995d10658
Vaibhav Kandwal <vaibhavkandwal@Vaibhavs-MacBook-Air.local>
2024-07-15 22:29:35 +0530

    2229

--- HashTables/HashTable.java
+++ HashTables/HashTable.java
@@ -1,5 +1,36 @@
 package HashTables;
 
-public class HashTable {
-    
+class Node<K, V> {
+    K key;
+    V value;
+    Node<K, V> next;
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
+
+}
+
+public class HashTable<K, V> {
+    private Node<K, V>[] buckets; // Array of linked list buckets for sepreate chaning
+    private int capacity; // Capacity of the hash table
+    private int size; // no of key value pairs
+
+    @SuppressWarnings("unchecked")
+    public HashTable(int capacity) {
+        this.capacity = capacity; // initail capacity
+        this.buckets = new Node[capacity]; // instailize buckets array
+        this.size = 0;// intialize size of hash table
+    }
+
+    // method to calculate index of a given key
+    private int getBucketIndex(K key) {
+        int hashcode = key.hashCode(); // gets hashcode of tge key
+        return Math.abs(hashcode) % capacity; // calculates the index using modulus operation
+    }
+
+    //Method to insert keyvalue pair into the hash table
+
 }
